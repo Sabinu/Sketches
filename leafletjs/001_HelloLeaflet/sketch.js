185517f8293a00ec7e6ba1ff14ddda2ebf3c4e84
Sabinu <sabin.purice@gmaill.com>
2016-11-13 09:31:47 +0200

    minor cleanup

--- leafletjs/001_HelloLeaflet/sketch.js
+++ leafletjs/001_HelloLeaflet/sketch.js
@@ -1,23 +1,20 @@
-var mymap = L.map('mapid').setView([44.43, 26.10], 13);
+var mymap = L.map('mapid').setView([44.43, 26.10], 13)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.satellite',
-    accessToken: 'pk.eyJ1Ijoic2FiaW51IiwiYSI6ImNpcjdyamZsODAwMXFpam1nOXl3bzl1djAifQ.T2RVI8AZuDpO8WXCAO64zQ'
-}).addTo(mymap);
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.satellite',
+  accessToken: 'pk.eyJ1Ijoic2FiaW51IiwiYSI6ImNpcjdyamZsODAwMXFpam1nOXl3bzl1djAifQ.T2RVI8AZuDpO8WXCAO64zQ'
+}).addTo(mymap)
 
+var marker = L.marker([44.43, 26.10]).addTo(mymap)
 
-var marker = L.marker([44.43, 26.10]).addTo(mymap);
-
-
-var popup = L.popup();
+var popup = L.popup()
 
 function onMapClick(event) {
-  popup
-    .setLatLng(event.latlng)
-    .setContent('You clicked the map at ' + event.latlng.toString())
-    .openOn(mymap);
+  popup.setLatLng(event.latlng)
+  .setContent('You clicked the map at ' + event.latlng.toString())
+  .openOn(mymap)
 }
 
 mymap.on('click', onMapClick);
